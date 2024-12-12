86239358e001c408bedac969b74f3f11c56b7af8
Haneena <pphaneena02@gmail.com>
2024-12-12 16:50:13 +0530

    token created for authentication

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,3 +1,4 @@
+const User = require("../models/userModels")
 const CustomError = require("../utils/customError")
 const { verifyToken } = require("../utils/jwt")
 
@@ -8,5 +9,18 @@ const authenticate=async(req,res,next)=>{
             throw new CustomError("Access token is missing",401)
         }
         const decoded=verifyToken(token,process.env.JWT_SECRET)
+        if(!decoded){
+            throw new CustomError("Invalid or expired refresh token",403)
+        }
+        const user=await User.findById(decoded.id)
+        if(!user){
+            throw new CustomError('User not found',404)
+        }
+        req.user=user
+        next(user)
+    }
+    catch(error){
+        next(error)
     }
 }
+module.exports=authenticate
